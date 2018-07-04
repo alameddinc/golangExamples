36f040de37f8f69f1333a66c141c4f9bd0fadf65
alameddince <2012010205039@ogrenci.karabuk.edu.tr>
2018-07-04 16:10:09 +0300

    edited Channels

--- channels.go
+++ channels.go
@@ -1,22 +1,31 @@
 package main
 
 import "fmt"
-  func sum(arr []int,c chan int )  {
+  func sumP(arr []int,c chan int )  {
     myTotal:=0
     for _,k:=range(arr){
       myTotal+=k
     }
-    c<-myTotal
+    c<-(myTotal*myTotal)
   }
 
 func main() {
   c:= make(chan int)
-  arr := []int{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19}
+  arr := makeRange(0, 10000)
 
-  go sum(arr[:len(arr)/6],c)
-  go sum(arr[len(arr)/3:len(arr)/3],c)
-  go sum(arr[len(arr)/6:],c)
+  go sumP(arr[:2*len(arr)/3],c)
+  go sumP(arr[len(arr)/3:len(arr)/3],c)
+  go sumP(arr[2*len(arr)/3:],c)
 
   x,y,z:=<-c,<-c,<-c
   fmt.Println(x+y+z)
+}
+
+
+func makeRange(min, max int) []int {
+    a := make([]int, max-min+1)
+    for i := range a {
+        a[i] = min + i
+    }
+    return a
 }
